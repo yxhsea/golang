318a06a946c30ef8e181097b444ed0d438099192
cyent <cyent@163.com>
2017-10-10 15:24:14 +0800

    Deployed 7bc5dcd with MkDocs version: 0.16.3

---
+++ js/baidu-tongji.js
@@ -0,0 +1,8 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?1c7b6b9434f8a47b73893004abb8a47d";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
